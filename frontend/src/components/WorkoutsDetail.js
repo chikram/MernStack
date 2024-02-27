1f3242dbe3fe41afec7b97dc0ed78480cceec192
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-28 00:45:31 +0500

    fetching data from backend

--- frontend/src/components/WorkoutsDetail.js
+++ frontend/src/components/WorkoutsDetail.js
@@ -1,12 +1,12 @@
 
-const WorkoutsDetail= ()=>{
+const WorkoutsDetail= ({prob})=>{
 return(
-    <div className="card" style={{width:"100%"}}>
+    <div className="card my-3" style={{width:"100%"}}>
   <div className="card-body">
-    <h5 className="card-title ">Card title</h5>
-    <p className="card-text">Some</p>
-    <p className="card-text">Some</p>
-    <p className="card-text">Some</p>
+    <h5 className="card-title text-danger fw-bold">{prob.title}</h5>
+    <p className="card-text"><strong>Load(kg):</strong><span className="mx-1">{prob.load}</span></p>
+    <p className="card-text"><strong>Reps:</strong><span className="mx-2">{prob.reps}</span></p>
+    <p className="card-text text-success">{prob.createdAt}</p>
   </div>
 </div>
 )
--- frontend/src/screen/Home.js
+++ frontend/src/screen/Home.js
@@ -1,12 +1,30 @@
+import { useState, useEffect } from "react"
 
 import WorkoutsDetail from "../components/WorkoutsDetail"
 
 
 function Home() {
+    const[workouts,setWorkouts]=useState(null)
+    useEffect(() => {
+     const fetchWorkouts=async()=>{
+      const response= await fetch('/api/workouts')
+     const json= await response.json()
+
+     if(response.ok){
+       setWorkouts(json)
+     }
+     }
+     fetchWorkouts()
+
+    }, [])
     return (
         <>
             <div className="container my-5">
-                <WorkoutsDetail />
+                {workouts && workouts.map((workout)=>(
+
+                    <WorkoutsDetail key={workout._id} prob={workout}/>
+                ))}
+                
             </div>
         </>
     )
