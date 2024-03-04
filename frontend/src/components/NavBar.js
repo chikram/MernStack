b262d500eb1c16c2f3b4b26e13e57f55a5c9819d
Ikram Aslam <ikramaslam1020@gmail.com>
2024-03-05 01:38:52 +0500

    some changes

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,7 +4,13 @@ function NavBar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-dark sticky-top">
       <div className="container-fluid mx-4">
         <Link className="navbar-brand fw-bold text-white" to="/">HealthMate</Link>
-        <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler bg-light"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
--- frontend/src/components/WorkoutsDetail.js
+++ frontend/src/components/WorkoutsDetail.js
@@ -1,12 +1,25 @@
 
 const WorkoutsDetail = ({ prob }) => {
+  const formateTimeStamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+    const date = new Date(timestamp);
+    const formatedDate = date.toLocaleString("en-us", options);
+    return formatedDate;
+  };
   return (
     <div className="card my-3" >
       <div className="card-body">
         <h5 className="card-title text-dark fw-bold">{prob.title}</h5>
         <p className="card-text"><strong>Load(kg):</strong><span className="mx-1">{prob.load}</span></p>
         <p className="card-text"><strong>Reps:</strong><span className="mx-2">{prob.reps}</span></p>
-        <p className="card-text text-success">{prob.createdAt}</p>
+        <p className="card-text text-success">{formateTimeStamp(prob.createdAt)}</p>
       </div>
     </div>
   )
